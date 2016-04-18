a10a4a097fb5ac0fd177b256e7e8388d38c858f5
Michał Kubrycz <michal.kubrycz@outlook.com>
2016-04-18 15:54:18 +0200

    add mster-detail toggle

--- dev/js/main.js
+++ dev/js/main.js
@@ -2,11 +2,17 @@ var casesApp = angular.module('casesApp', []);
 
 casesApp.controller('CasesListCtrl', function ($scope, $http) {
 	$scope.cases = null;
-	$http.get('sprawy.json')
-		.success(function(data) {
-			$scope.cases = data;
-		})
-		.error(function(data, status, error, config) {
-			$scope.cases = [{heading:"Error",description:"Could not load json   data"}];
-		});
+	$scope.loadCases = function() {
+		$http.get('sprawy.json', {cache: true})
+			.success(function(data) {
+				$scope.cases = data;
+			})
+			.error(function(data, status, error, config) {
+				$scope.cases = [{heading:"Error",description:"Could not load json   data"}];
+			});
+		};
+	$scope.visible = false;
+	$scope.toggle = function() {
+		$scope.visible = !$scope.visible;
+	};
 });
